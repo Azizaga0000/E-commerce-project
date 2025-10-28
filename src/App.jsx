a731e638451b37c1e242c5eb777b78971d6c7aa5
Azizaga0000 <dev.bash.wrld@outlook.com>
2025-10-28 23:19:42 +0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AppRoutes from './components/AppRoutes'
+import { AuthProvider } from './components/AuthContext'
+
+function App() {
+
+  return (
+    <>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
+    </>
+  )
+}
+
+export default App
